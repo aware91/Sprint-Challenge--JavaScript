d174b3be1f13581196d97a29d3013d164895241c
Anthony Ware <aware91coding@gmail.com>
2020-02-21 21:20:10 -0600

    got universities done

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -52,7 +52,7 @@ const graduates = [
 /* Request 1: Create a new array called universities that contains all the universities in the graduates array. This will be an array of strings.
 
 Once you have the new array created, sort the universities alphabetically and log the result. */
-const universities = graduates.map((universities) => {
+const universities = graduates.map((graduates) => {
   return {'unversity': graduates.university};
 }); 
 console.log(universities);
